66a62182a1cf9962156ad47323f9f301479efb68
Vladimir Komendantskiy <komendantsky@gmail.com>
2018-04-18 13:02:56 +0100

    supplied absolute paths to protoc_rust::run

--- build.rs
+++ build.rs
@@ -1,10 +1,27 @@
+use std::env;
 extern crate protoc_rust;
 
-fn main() {
-    println!("cargo:rerun-if-changed=proto/message.proto");
+fn generate_proto_rs() {
+    let current_dir = env::current_dir().unwrap();
+    let protoc_out_dir = current_dir.join("src").join("proto");
+    let protoc_input = current_dir.join("proto").join("message.proto");
+    let protoc_output = current_dir.join("src").join("proto")
+        .join("message.rs");
+    let rerun_on_input_change =
+        format!("cargo:rerun-if-changed={}", protoc_input.to_str().unwrap());
+    let rerun_on_output_change =
+        format!("cargo:rerun-if-changed={}", protoc_output.to_str().unwrap());
+
+    println!("{}", rerun_on_input_change);
+    println!("{}", rerun_on_output_change);
+
     protoc_rust::run(protoc_rust::Args {
-        out_dir: "src/proto",
-        input: &["proto/message.proto"],
-        includes: &["proto"],
+        out_dir: protoc_out_dir.to_str().unwrap(),
+        input: &[protoc_input.to_str().unwrap()],
+        includes: &[],
     }).expect("protoc");
+}
+
+fn main() {
+    generate_proto_rs();
 }
